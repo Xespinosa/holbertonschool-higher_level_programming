d430341164c69ceeb078718189f152b9b5cb7d7a
Crimson Cross <betancourtxavier77@gmail.com>
2023-06-20 16:02:47 -0400

    comment fix

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+"""class MyList that inherits from list
+"""
+
 
 class Mylist(list):
+    """Public instance method
 
+    Args:
+        list (list): list of elements type int
+    """
     def print_sorted(self):
-
+        """prints the list, but sorted (ascending sort)
+        """
         print(sorted(self))
