6bce95812cbcf5e6f1452d3d3612fe708d5511d0
Crimson Cross <betancourtxavier77@gmail.com>
2023-06-21 10:09:45 -0400

    uploading file 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""_summary_
+"""
+
+
+def inherits_from(obj, a_class):
+    """_summary_
+
+    Args:
+        obj (_type_): _description_
+        a_class (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+
+    return issubclass(obj, a_class)
