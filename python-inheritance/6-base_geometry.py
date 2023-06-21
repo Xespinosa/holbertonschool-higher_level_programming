a5026532ec60865f9a611bcb53a78520c643b055
Crimson Cross <betancourtxavier77@gmail.com>
2023-06-21 10:51:13 -0400

    uploading file 6 and 6-main

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""module for a class called Base Geometry with public instance method area
+"""
+
+
+class BaseGeometry:
+    """class with an exception
+    """
+    def area(self):
+        """defines the area of the geometry
+
+        Raises:
+            Exception: error message for user
+        """
+        raise Exception('area() is not implemented')
