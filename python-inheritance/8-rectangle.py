70cde44d4e96ff24d8bdc6ca0a7b4881b049ae65
Crimson Cross <betancourtxavier77@gmail.com>
2023-06-21 14:30:02 -0400

    uploading file 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -22,3 +22,13 @@ class BaseGeometry:
                 raise TypeError(f"{name} must be an integer")
             if value <= 0:
                 raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self._width = width
+        self._height = height
+        
