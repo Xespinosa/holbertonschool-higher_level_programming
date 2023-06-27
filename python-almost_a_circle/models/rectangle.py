fe7b3e9ee098ed18689e953abc0eb810421a6bc4
Crimson Cross <betancourtxavier77@gmail.com>
2023-06-27 15:59:17 -0400

    fixes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -6,7 +6,7 @@ from models.base import Base
 
 class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
-        """_summary_
+        """initialization
 
         Args:
             width (_type_): _description_
